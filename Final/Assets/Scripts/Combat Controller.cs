30b7799021774cb5b71759afa68a633d5d0659b6
PlatypusFries <kevin.martens@live.com>
2015-06-02 21:37:11 -0700

    -- began work on combat system

---
+++ Final/Assets/Scripts/Combat Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatController : MonoBehaviour {
+	ArrayList combatantList = new ArrayList();
+
+	public GameObject combatant01;
+	public GameObject combatant02;
+	public GameObject combatant03;
+
+
+	void Start () {
+		combatantList.Add (combatant01);
+		combatantList.Add (combatant02);
+		combatantList.Add (combatant03);
+
+		foreach (GameObject combatant in combatantList)
+		{
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Final/Assets/Scripts/Combatant.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combatant : MonoBehaviour {
+
+	public string name = "void";
+	public float timer = 500;
+	public bool controllable = false;
+
+	float timerProgress;
+
+}
