c66a64ace6bfc38d99944a6a7b4f02e26433aa4d
Kevin Martens <kevin.martens@live.com>
2015-06-03 20:11:05 -0700

    Combat Sequence
    - Base code for combat sequence finished

---
+++ Final/Assets/Scripts/CombatController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatController : MonoBehaviour {
+	ArrayList combatantList = new ArrayList();
+
+	public GameObject combatant01;
+	public GameObject combatant02;
+	public GameObject combatant03;
+
+    bool turnPaused = false;
+
+
+	void Start () {
+		combatantList.Add (combatant01);
+		combatantList.Add (combatant02);
+		combatantList.Add (combatant03);
+
+		foreach (GameObject combatant in combatantList)
+		{
+            Combatant unit = combatant.GetComponent<Combatant>();
+            unit.timerProgress = Random.Range(0, unit.timer);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!turnPaused)
+        {
+            foreach (GameObject combatant in combatantList)
+            {
+                Combatant unit = combatant.GetComponent<Combatant>();
+                unit.timerProgress += 1000 * Time.deltaTime;
+                if (unit.timerProgress >= unit.timer)
+                {
+                    turnPaused = true;
+                    Debug.Log("PAUSE");
+                    unit.DoAction();
+                }
+            }
+        }
+	}
+
+    public void ResumeCombat()
+    {
+        turnPaused = false;
+    }
+}
--- Final/Assets/Scripts/Combatant.cs
+++ Final/Assets/Scripts/Combatant.cs
@@ -3,10 +3,40 @@ using System.Collections;
 
 public class Combatant : MonoBehaviour {
 
-	public string name = "void";
+    public GameObject controllerObject;
+
+    
+    // Set the name that will appear in combat here. If void appears this wasn't set.
+	public string characterName = "void";
+
+    // Set the character's total timer, in milliseconds. This will be changed to
+        // a ratio based on speed at some point in the future. 500 ms is the current default.
+           // A lower number means a faster character.
 	public float timer = 500;
+
+    // Whether this is a player character or NPC. NPC is the default.
 	public bool controllable = false;
 
-	float timerProgress;
+    // How far into the timer this current character is.
+	public float timerProgress;
+
+    bool acting = false;
+
+    public void DoAction()
+    {
+        Debug.Log( name + "'s turn to act. Press enter to do something.");
+        acting = true;
+    }
+
+   void Update ()
+    {
+        if (acting && Input.GetKeyDown(KeyCode.Return))
+        {
+            CombatController controller = controllerObject.GetComponent<CombatController>();
+            timerProgress = 0;
+            acting = false;
+            controller.ResumeCombat();
+        }
+    }
 
 }
