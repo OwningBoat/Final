662d63c8611d10dd77e1e8a7cd2be87f12cc7c01
Aaron Gilbert <marth8880@gmail.com>
2015-06-09 22:50:30 -0700

    Attack and timer managers, GUI stuff too

---
+++ Final/Assets/Scripts/GUIAttackManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIAttackManager : MonoBehaviour
+{
+    public GameObject Timer;
+
+    public void AttackClicked(int intMember)
+    {
+        // TODO: Create DoAttack() method in CombatController.cs
+        //DoAttack( intMember );
+        Debug.Log( "Character " + intMember.ToString() + " DoAttack()" );
+        //Timer = Timer.GetComponent<GUITimerManager>;
+        //GetComponent<GUITimerManager>
+    }
+}
---
+++ Final/Assets/Scripts/GUITimerManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUITimerManager : MonoBehaviour
+{
+    static public GUITimerManager Instance;
+
+    Text Text_PartyTime;
+
+    public GameObject AttackButton;
+
+    public float curTime = 0f;
+    public float maxTime = 100f;
+    public float fSpeed = 9f;
+
+
+	void Awake()
+    {
+        Instance = this;
+        Text_PartyTime = GetComponent<Text>();
+        Text_PartyTime.text = curTime.ToString() + " / " + maxTime.ToString();
+    }
+
+    void Update()
+    {
+        curTime += fSpeed * Time.deltaTime;
+
+        if (curTime > 99)
+        {
+            curTime = 100f;
+            AttackButton.SetActive( true );
+        }
+        else
+        {
+            AttackButton.SetActive( false );
+        }
+
+        Text_PartyTime.text = curTime.ToString() + " / " + maxTime.ToString();
+    }
+}
